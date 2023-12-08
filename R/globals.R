0f0c970259cc38ad5e072739fd9136852445f95c
Kittipos <ki11ip0.s.a.s@gmail.com>
2023-12-08 21:57:58 +0700

    Update global vars

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 utils::globalVariables(c(".data", "method", "n", "p.value", "shapiro_p.value", "shapiro_statistic",
-                         "PC1", "PC2", "component", "terms", "value"))
+                         "PC1", "PC2", "component", "term", "terms", "value"))
